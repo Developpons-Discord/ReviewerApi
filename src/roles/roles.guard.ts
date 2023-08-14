8186a7e4298212c55b4b134a5447c89912ed00c8
Tom BUTIN <tom.frefre.13@gmail.com>
2023-08-14 17:07:07 +0200

    Fixed styling

--- src/roles/roles.guard.ts
+++ src/roles/roles.guard.ts
@@ -1,7 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from './roles.decorator';
-import { FullUser } from '../users/user.model';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
